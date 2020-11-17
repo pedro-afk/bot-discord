240ab88995db7612469e423c1e6ea70cd3c0466a
unknown <ryangnr123@outlook.com>
2020-11-16 21:45:07 -0300

    fix: update security token location

--- src/server.js
+++ src/server.js
@@ -1,36 +1,31 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+require('dotenv').config()
 
-const accessBot = require('./config/botConfig.json');
+const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(
         ch => ch.name === 'member-log'
     );
     if (channel) return;
-    // Send the message, mentioning the member
     channel.send(`Seja bem vindo ao server, ${member}`);
 });
 
 client.on('ready', () => {
-  console.log(`Bot ready, ${client.user.tag}!`);
+    console.log(`Bot ready, ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
-  }
-  else if(msg.content == '!ola' || msg.content == '!oi'){
-    msg.reply(`Olá, como vai?`);
-  }
-  else if(msg.content === '!avatar') {
-    // Send the user's avatar URL
-    msg.reply(msg.author.displayAvatarURL());
-  }
-  else if (msg.content === '!rip') {
-    const attachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
-    msg.channel.send(`${msg.author},`, attachment);
-}
+    if (msg.content === '!ping') {
+        msg.reply('Pong!');
+    } else if (msg.content == '!ola' || msg.content == '!oi') {
+        msg.reply(`Olá, como vai?`);
+    } else if (msg.content === '!avatar') {
+        msg.reply(msg.author.displayAvatarURL());
+    } else if (msg.content === '!rip') {
+        const attachment = new Discord.MessageAttachment('https://i.imgur.com/w3duR07.png');
+        msg.channel.send(`${msg.author},`, attachment);
+    }
 });
 
-client.login(accessBot.token);
+client.login(process.env.TOKEN);
